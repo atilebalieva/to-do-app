add5f5489317d65b63cf2af5f093b9aad32ac000
atilebalieva <aitilebalieva@gmail.com>
2024-02-08 07:14:31 -0600

    Fixed bugs.

--- To-do-board_files/js/script.js
+++ To-do-board_files/js/script.js
@@ -1,9 +1,28 @@
-const toDo = document.getElementById("to-do");
-const inProgress = document.getElementById("in-progress");
-const done = document.getElementById("done");
-const listOfTasks = document.querySelectorAll(".list_tasks");
+const todoColumn = document.getElementById("todo");
+const inProgressColumn = document.getElementById("in-progress");
+const doneColumn = document.getElementById("done");
 
-/* Sidebar toggle */
+window.addEventListener("click", addNewTaskHandler);
+
+const taskList = document.querySelectorAll(".list_tasks");
+taskList.forEach((column) => {
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+
+    const draggableTask = document.querySelector(".dragging");
+
+    if (draggableTask) {
+      column.appendChild(draggableTask);
+
+      saveTaskData(todoColumn, "todo");
+      saveTaskData(inProgressColumn, "in-progress");
+      saveTaskData(doneColumn, "done");
+    }
+  });
+});
+
+// Sidebar toggle - START
+
 function sidebarToggle(display, width) {
   document.querySelector(".sidebar_close-form").style.display = display;
   document.getElementById("sidebar_open").style.display = display;
@@ -21,9 +40,10 @@ document.getElementById("sidebar_open").addEventListener("click", () => {
   document.querySelector(".sidebar_none").classList.add("sidebar");
   document.querySelector(".sidebar").classList.remove("sidebar_none");
 });
-/* Sidebar toggle */
 
-function setTaskData(taskColumn, storageKey) {
+// Sidebar toggle - END
+
+function saveTaskData(taskColumn, storageKey) {
   const tasks = Array.from(taskColumn.querySelectorAll(".draggable-task")).map(
     (item) => item.querySelector("textarea").textContent
   );
@@ -35,115 +55,120 @@ function removeEmptyTasks(taskColumn, storageKey) {
   const draggableTasks = taskColumn.querySelectorAll(
     ".list_tasks .draggable-task"
   );
-
   draggableTasks.forEach((draggableTask) => {
     const textarea = draggableTask.querySelector(".textarea.task");
     if (textarea && textarea.value.trim() === "") {
       draggableTask.remove();
-      setTaskData(taskColumn, storageKey);
+      saveTaskData(taskColumn, storageKey);
     }
   });
 }
 
-function addEventListenersToTask(taskColumn, storageKey) {
-  const changeTask = taskColumn.querySelectorAll(".list_tasks .task");
+function addTaskEventListeners(taskColumn) {
+  const storageKey = taskColumn.id;
+  const listTasks = taskColumn.querySelector(".list_tasks");
 
-  changeTask.forEach((item) => {
-    item.addEventListener("input", (e) => {
-      item.innerText = e.target.value;
-      setTaskData(taskColumn, storageKey);
+  listTasks.addEventListener("input", (e) => {
+    const taskElement = e.target.closest(".task");
+    if (taskElement) {
+      taskElement.innerText = e.target.value;
+      saveTaskData(taskColumn, storageKey);
       removeEmptyTasks(taskColumn, storageKey);
-    });
+    }
   });
 
   const draggablesTasks = document.querySelectorAll(".draggable-task");
   draggablesTasks.forEach((draggable) => {
+    console.log("draging");
     draggable.addEventListener("dragstart", () => {
       draggable.classList.add("dragging");
     });
+
     draggable.addEventListener("dragend", () => {
       draggable.classList.remove("dragging");
+      saveTaskData(taskColumn, storageKey);
     });
   });
 }
 
-function getTaskData(taskColumn, storageKey) {
-  const data = JSON.parse(localStorage.getItem(storageKey));
+function addNewTaskHandler(e) {
+  function removeTaskAddElements() {
+    taskSaveButton.remove();
+    addCardButton.classList.remove("remove-add-btn");
+  }
 
-  if (data) {
-    data.forEach((item) => {
-      const taskName = `
+  if (e.target.dataset.role !== "add-task") return;
+
+  const taskColumn = e.target.closest(".task_column");
+  const taskList = taskColumn.querySelector(".list_tasks");
+  const addCardButton = taskColumn.querySelector(".task_column-add-btn");
+
+  taskList.insertAdjacentHTML("beforeend", getTaskCardHtml());
+
+  const taskSaveButton = taskColumn.querySelector(".add-task_card");
+  const cancelButton = taskColumn.querySelector(".remove-task_card");
+
+  addCardButton.classList.add("remove-add-btn");
+  cancelButton.addEventListener("click", removeTaskAddElements);
+
+  taskSaveButton.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const taskValue = taskColumn.querySelector(".task-value").value.trim();
+
+    if (!taskValue) return;
+
+    taskList.insertAdjacentHTML("beforeend", createTaskText(taskValue));
+    taskList.classList.add("height");
+
+    addTaskEventListeners(taskColumn);
+
+    saveTaskData(todoColumn, "todo");
+    saveTaskData(inProgressColumn, "in-progress");
+    saveTaskData(doneColumn, "done");
+
+    removeTaskAddElements();
+  });
+}
+
+function getTaskCardHtml() {
+  return `
+    <form class="add-task_card" action="#">
+      <textarea name="text" class="textarea task-value"cols="28" rows="10" placeholder="New task..." style="height: 70px;" ></textarea>
+      <button type="submit" class="add-task_card-btn">Add card</button>
+      <span class="remove-task_card">Cancel</span>
+    </form>`;
+}
+
+function createTaskText(taskValue) {
+  console.log(true);
+  return `
+    <div class="draggable-task" draggable="true">
+      <textarea name="text" class="textarea task">${
+        taskValue.charAt(0).toUpperCase() + taskValue.slice(1)
+      }</textarea>
+    </div>`;
+}
+
+function loadTaskData(taskColumn) {
+  const data = JSON.parse(localStorage.getItem(taskColumn.id));
+  if (!data) return;
+
+  data.forEach((item) => {
+    const taskName = `
       <div class="draggable-task" draggable="true">
-        <textarea class="textarea task" style={height: "fit-content"}>${item}</textarea>
+        <textarea name="text" class="textarea task">${item}</textarea>
       </div>`;
 
-      taskColumn
-        .querySelector(".list_tasks")
-        .insertAdjacentHTML("beforeend", taskName);
-      addEventListenersToTask(taskColumn, storageKey);
-    });
-  }
+    taskColumn
+      .querySelector(".list_tasks")
+      .insertAdjacentHTML("beforeend", taskName);
+  });
+
+  addTaskEventListeners(taskColumn);
 }
 
-window.addEventListener("click", (e) => {
-  if (e.target.dataset.role === "add-task") {
-    function doNotAddTask() {
-      addTaskCard.remove();
-      taskColumnAddBtn.classList.remove("remove-add-btn");
-    }
-
-    const taskColumn = e.target.closest(".task_column");
-    const listOfTasks = taskColumn.querySelector(".list_tasks");
-    const storageKey = taskColumn.id;
-    const addTaskCardHtml = `<form class="add-task_card" action="#"><textarea class="textarea task-value"cols="28" rows="10" placeholder="Enter a title for this card..."
-      style="height: 56px;" ></textarea>
-    <button type="submit" class="btn btn-primary add-task_card-btn">Add card</button>
-    <span class="remove-task_card">x</span></form>`;
-
-    listOfTasks.insertAdjacentHTML("beforeend", addTaskCardHtml);
-    const taskColumnAddBtn = taskColumn.querySelector(".task_column-add-btn");
-    taskColumnAddBtn.classList.add("remove-add-btn");
-
-    const addTaskCard = taskColumn.querySelector(".add-task_card");
-    const taskValue = taskColumn.querySelector(".task-value");
-
-    const removeTaskCard = taskColumn.querySelector(".remove-task_card");
-    removeTaskCard.addEventListener("click", doNotAddTask);
-
-    addTaskCard.addEventListener("submit", (e) => {
-      e.preventDefault();
-      if (taskValue.value) {
-        const taskName = `
-        <div class="draggable-task" draggable="true">
-          <textarea class="textarea task" style={height: "fit-content"}>${
-            taskValue.value.charAt(0).toUpperCase() + taskValue.value.slice(1)
-          }</textarea>
-        </div>`;
-
-        listOfTasks.insertAdjacentHTML("beforeend", taskName);
-        listOfTasks.classList.add("height");
-        setTaskData(toDo, "to-do");
-        setTaskData(inProgress, "in-progress");
-        setTaskData(done, "done");
-
-        doNotAddTask();
-      }
-      addEventListenersToTask(taskColumn, storageKey);
-    });
-  }
-});
-
-listOfTasks.forEach((column) => {
-  column.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    const draggableTask = document.querySelector(".dragging");
-    column.appendChild(draggableTask);
-    setTaskData(toDo, "to-do");
-    setTaskData(inProgress, "in-progress");
-    setTaskData(done, "done");
-  });
-});
-
-getTaskData(toDo, "to-do");
-getTaskData(inProgress, "in-progress");
-getTaskData(done, "done");
+// Load saved data from the local storage.
+loadTaskData(todoColumn);
+loadTaskData(inProgressColumn);
+loadTaskData(doneColumn);
